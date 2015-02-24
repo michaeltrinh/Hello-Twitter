a7d7205e6186c240e3ad591f04402219ccb85cc3
Ankur Huralikoppi <ankurh@ymail.com>
2015-02-23 22:19:54 -0800

    API checking for retweets and user details

--- auth.py
+++ auth.py
@@ -14,16 +14,28 @@ for line in file:
     elif constants.CONSUMER_KEY in line:
         consumer_key = line.split(":")[1].strip()
 
-print ("Access Token: %s"
+"""print ("Access Token: %s"
        "Access Token Secret: %s"
        "Consumer Key: %s"
        "Consumer Secret: %s"
        %(access_token_key, access_token_secret, consumer_key, consumer_secret))
-
+"""
 api = twitter.Api(consumer_key=consumer_key,
         consumer_secret=consumer_secret,
         access_token_key=access_token_key,
         access_token_secret=access_token_secret)
 
-users = api.GetFriends()
-print [u.name for u in users]
+user = api.GetUser(screen_name='ankurdh')
+
+print ("***** User stats *****")
+print ("Name            : %s" %user.GetName())
+print ("Description     : %s" %user.GetDescription())
+print ("Follower Count  : %d" %user.GetFollowersCount())
+print ("Following Count : %d" %user.GetFriendsCount())
+print ("User ID         : %d" %user.GetId())
+
+print ("***** Recent User Retweets *****")
+retweets=api.GetUserRetweets()
+
+for retweet in retweets:
+    print retweet.GetText()
